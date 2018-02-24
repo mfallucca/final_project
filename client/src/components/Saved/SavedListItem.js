337bc928a6c67ae135cb2a05d22af4ac864dc6b6
Matthew Fallucca <mfallucca@Ashleys-MacBook-Pro.local>
2018-02-24 11:42:47 -0600

    removing old stuff

--- client/src/components/Saved/SavedListItem.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const SavedListItem = props =>
-  <li className="list-group-item">
-    {props.children}
-  </li>;
--- client/src/components/Saved/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./SavedList";
-export * from "./SavedListItem";
