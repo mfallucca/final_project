aee2d1abb3c6eca38696d47d553e2efb32933122
Matthew Fallucca <mfallucca@Ashleys-MacBook-Pro.local>
2018-02-24 13:12:00 -0600

    adding amazon stuff

---
+++ server/routes/amazonAPIinfo.js
@@ -0,0 +1,7 @@
+var amazon = require('amazon-product-api');
+
+var client = amazon.createClient({
+  awsId: "AKIAI2DDSRFYAR4GCWRQ",
+  awsSecret: "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",
+  awsTag: "mfallucca-20"
+});
