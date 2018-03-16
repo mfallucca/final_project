7fc2ee77d7895b86246b17ed1b12f139506989db
unknown <matthew.fallucca@gmail.com>
2018-03-16 15:30:45 -0500

    adding comments to API file

--- server/routes/api.js
+++ server/routes/api.js
@@ -22,13 +22,11 @@ router.get('/dashboard', (req, res) => {
     // user values passed through from auth middleware
     user: req.user
   });
-  // console.log(req.user)
 });
 
 // Amazon API route, takes UPC as a parameter
 
 router.get('/amazon/:upc', (req, res) => {
-  // console.log(res)
   let upc = req.params.upc;
 
   client.itemLookup({
@@ -74,43 +72,43 @@ router.get('/walmart/:search', (req, res) => {
       queryResults: resultsArray
     });
   })
-
 })
 
 // Ebay API route, takes UPC as a parameter
 // I removed the ebayobject, though we may need to add it for error handling
 
 router.get('/ebay/:upc', (req, res) => {
-let upc = req.params.upc;
-let params = {
-  'OPERATION-NAME': 'findItemsAdvanced', 
-  'keywords': upc,
-  'sortOrder': 'PricePlusShippingLowest'
-};
+  let upc = req.params.upc;
+  let params = {
+    'OPERATION-NAME': 'findItemsAdvanced', 
+    'keywords': upc,
+    'sortOrder': 'PricePlusShippingLowest'
+  };
 
-ebay.get('finding', params, function (err, data) {
-  if(err) throw err;
-  if (data && data.findItemsAdvancedResponse && data.findItemsAdvancedResponse[0].searchResult && data.findItemsAdvancedResponse[0].searchResult[0].item && data.findItemsAdvancedResponse[0].searchResult[0].item[0].title && data.findItemsAdvancedResponse[0].searchResult[0].item[0].galleryURL && data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus && data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus[0].convertedCurrentPrice && data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo && data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo[0].shippingServiceCost && data.findItemsAdvancedResponse[0].searchResult[0].item[0].viewItemURL) {
-    res.status(200).json({
-      ebayTitle: data.findItemsAdvancedResponse[0].searchResult[0].item[0].title[0],
-      ebayImage: data.findItemsAdvancedResponse[0].searchResult[0].item[0].galleryURL[0],
-      ebayPrice: data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus[0].convertedCurrentPrice[0].__value__,
-      ebayShipping: data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo[0].shippingServiceCost[0].__value__,
-      ebayURL: data.findItemsAdvancedResponse[0].searchResult[0].item[0].viewItemURL[0]
-    });
-  }
-  else {
-    res.status(200).json({
-      ebayTitle: '',
-      ebayImage: '',
-      ebayPrice: '',
-      ebayShipping: '',
-      ebayURL: ''
-    });
-  }
-});
+  ebay.get('finding', params, function (err, data) {
+    if(err) throw err;
+    if (data && data.findItemsAdvancedResponse && data.findItemsAdvancedResponse[0].searchResult && data.findItemsAdvancedResponse[0].searchResult[0].item && data.findItemsAdvancedResponse[0].searchResult[0].item[0].title && data.findItemsAdvancedResponse[0].searchResult[0].item[0].galleryURL && data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus && data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus[0].convertedCurrentPrice && data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo && data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo[0].shippingServiceCost && data.findItemsAdvancedResponse[0].searchResult[0].item[0].viewItemURL) {
+      res.status(200).json({
+        ebayTitle: data.findItemsAdvancedResponse[0].searchResult[0].item[0].title[0],
+        ebayImage: data.findItemsAdvancedResponse[0].searchResult[0].item[0].galleryURL[0],
+        ebayPrice: data.findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus[0].convertedCurrentPrice[0].__value__,
+        ebayShipping: data.findItemsAdvancedResponse[0].searchResult[0].item[0].shippingInfo[0].shippingServiceCost[0].__value__,
+        ebayURL: data.findItemsAdvancedResponse[0].searchResult[0].item[0].viewItemURL[0]
+      });
+    }
+    else {
+      res.status(200).json({
+        ebayTitle: '',
+        ebayImage: '',
+        ebayPrice: '',
+        ebayShipping: '',
+        ebayURL: ''
+      });
+    }
+  });
 });
 
+// Saved API route, takes email and searchTerm as parameters.  This is used to save search terms to each account
 
 router.get('/saved/:email/:searchTerm', (req, res, next) => {
   let currentEmail = req.params.email
@@ -118,16 +116,18 @@ router.get('/saved/:email/:searchTerm', (req, res, next) => {
   db.findOneAndUpdate(
     { email: currentEmail },
     { $push: { saved: currentSearch } }
- ).then(function(results) {
+  ).then(function(results) {
   console.log(results.saved);
-  res.status(200).json({
-    savedResults: results.saved
+    res.status(200).json({
+      savedResults: results.saved
+    })
   })
-})
-.catch(function(err) {
-  console.log(err.message);
+  .catch(function(err) {
+    console.log(err.message);
+  });
 });
-});
+
+// Retrieval API route, takes email as a parameter.  This is used to retrieve the previously searched items for each account
 
 router.get('/retrieve/:email', (req, res, next) => {
   let currentEmail = req.params.email
@@ -146,16 +146,5 @@ router.get('/retrieve/:email', (req, res, next) => {
   })
 })
 
-// http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=MatthewF-finalpro-PRD-de44c9784-2b97810c&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&globalId=EBAY-US&keywords=633472002908&sortOrder=PricePlusShippingLowest
 
 module.exports = router;
-
-
-
-
-// let searchItem = 'external hard drive'
-// const resultsArray = []
-// walmart.search(searchItem).then(function(items) {
-//     resultsArray.push(items);
-//     console.log(JSON.stringify(resultsArray, null, 2));
-// });
